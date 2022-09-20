610f761efef3a81018db4ecff69f1cf018ab7a35
kasahh <bunmiwhesu@yahoo.com>
2022-09-20 19:40:14 +0000

    pushing code for 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     newstr = [i for i in str]
-    print(newstr)
-    del[n]
-    print(newstr)
+    if n < len(newstr) and n > 0:
+        del newstr[n]
     finstr = ""
     for i in newstr:
         finstr += i
     return finstr
-
