1acfe17dc2cbca3a5683f415569b9c9833133aeb
kasahh <bunmiwhesu@yahoo.com>
2022-09-20 19:43:09 +0000

    fix for 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     newstr = [i for i in str]
-    if n < len(newstr) and n > 0:
+    if n < len(newstr) and n >= 0:
         del newstr[n]
     finstr = ""
     for i in newstr:
