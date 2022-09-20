4011b957681db7d43bd7517b5e94dda0ee26fb16
kasahh <bunmiwhesu@yahoo.com>
2022-09-20 12:27:53 +0000

    fixes for 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i != 0:
-        print(",",end=" ")
-    if i < 10:
-        print("0{}".format(i), end="")
-    else:
-        print("{}".format(i), end="")
+    print("{0:02d}".format(i), end=", ")
 print("\n")
