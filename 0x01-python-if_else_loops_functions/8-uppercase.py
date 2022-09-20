bb7c4f46332ba4b437b80664f66382cd91698f34
kasahh <bunmiwhesu@yahoo.com>
2022-09-20 18:24:00 +0000

    printing last digit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,6 @@ def uppercase(str):
         if (y >= 97) and (y <= 122):
             y -= 32
             # str = str.replace(str[i],chr(y))
-            str = "{}".format(str.replace(str[i],chr(y)))
-    
+            str = "{}".format(str.replace(str[i], chr(y)))
     print("{}".format(str))
     return str
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        number = abs(number)
+    while number >= 10:
+        number %= 10
+    print(number, end="")
+    return number
+
