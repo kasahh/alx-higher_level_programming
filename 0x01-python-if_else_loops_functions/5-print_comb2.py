0fc7d21791ba8268d39e0c3ee6a775b6b0b673d5
kasahh <bunmiwhesu@yahoo.com>
2022-09-20 12:05:53 +0000

    f to format fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print(f"{i:02d}".format, end=', ')
+    if i != 0:
+        print(",",end=" ")
+    if i < 10:
+        print("0{}".format(i), end="")
+    else:
+        print("{}".format(i), end="")
 print("\n")
