091818a5cca5f3ccfb40eac2544d0959bb7ae3ec
kasahh <bunmiwhesu@yahoo.com>
2022-09-22 12:29:24 +0000

    infinite addition in python

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    arr = sys.argv
+    larr = len(sys.argv)
+    sum = 0 
+
+    if larr <= 1:
+        print(sum)
+    else:
+        for i in arr:
+            if i != arr[0]:
+                sum += int(i)
+        print(sum)
+            
+
+
+if __name__ == "__main__":
+    main()
