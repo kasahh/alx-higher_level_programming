f05e0fe57dc497eb82c78a71fdaa8396f3f04cfd
kasahh <bunmiwhesu@yahoo.com>
2022-10-11 20:34:43 +0000

    fixes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""empty class square"""
 
 class Square:
+    """empty class square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""class Square with instance size"""
 
 class Square:
+    """class creates Squares using its size"""
 
     def __init__(self, size):
+        """constructor of the square"""
         self.__size = size
